db092715c8a037972f3a32dbf4b31139bd50a70d
pomman079 <pomman079@innodep.com>
2022-08-21 19:02:40 +0900

    성격유형검사 문제풀이 * https://school.programmers.co.kr/learn/courses/30/lessons/118666?language=cpp# * map 사용

---
+++ programmers/level1/C30L118666.h
@@ -0,0 +1,54 @@
+#pragma once
+//level 1, https://school.programmers.co.kr/learn/courses/30/lessons/118666?language=cpp
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+class C30L118666 {
+public:
+	void makeScore(std::string& type, int& score) {
+		if (score == 4)	return;		//It doesn't make any sense.
+
+		int tempScore = 0;
+		std::string tempType("");
+		if (score >= 1 && score <= 3) {
+			tempType = type.substr(0, 1);
+			if (score == 1)
+				tempScore = 3;
+			else if (score == 2)
+				tempScore = 2;
+			else if (score == 3)
+				tempScore = 1;
+		}
+		else if (score >= 5 && score <= 7) {
+			tempType = type.substr(1, 1);
+			if (score == 7)
+				tempScore = 3;
+			else if (score == 6)
+				tempScore = 2;
+			else if (score == 5)
+				tempScore = 1;
+		}
+		table[tempType] += tempScore;
+	}
+
+    std::string solution(std::vector<std::string> survey, std::vector<int> choices) {
+        std::string answer = "";
+ 
+		if (survey.size() <= 1000) {	//One of the limitations
+			for (int i = 0; i < survey.size(); i++) {
+				makeScore(survey[i], choices[i]);
+			}
+		}
+
+		if (table["R"] < table["T"]) { answer += "T"; }		else { answer += "R"; }
+		if (table["C"] < table["F"]) { answer += "F"; }		else { answer += "C"; }
+		if (table["J"] < table["M"]) { answer += "M"; }		else { answer += "J"; }
+		if (table["A"] < table["N"]) { answer += "N"; }		else { answer += "A"; }
+
+		std::cout << "Answer: " << answer << std::endl;
+        return answer;
+    }
+private:
+    std::map<std::string, int> table;
+};
---
+++ programmers/programmers.cpp
@@ -0,0 +1,27 @@
+﻿// programmers.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+
+using namespace std;
+
+#pragma region LEVEL_1
+#include "level1/C30L118666.h"  
+#pragma endregion
+
+#pragma region LEVEL_2
+
+#pragma endregion
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    C30L118666 C30L118666;
+    C30L118666.solution({ "AN", "CF", "MJ", "RT", "NA" }, { 5, 3, 2, 7, 5 });
+}
